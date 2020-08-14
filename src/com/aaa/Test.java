40aadc1e240cd66e7951d5aedeeab9b457d00f19
chy <3244305897@example.com>
2020-08-14 19:13:19 +0800

    小组人员名单

--- src/com/aaa/Test.java
+++ src/com/aaa/Test.java
@@ -3,6 +3,10 @@ package com.aaa;
 public class Test {
     public static void main(String[] args) {
         System.out.println("刘正阳");
+        System.out.println("刘申奥");
+        System.out.println("程郝毅");
+        System.out.println("闫鹏冲");
+        System.out.println("房安迪");
         System.out.println("第二次测试");
         System.out.println("第三次测试");
     }
